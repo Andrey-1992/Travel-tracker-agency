d48caab313178c805a35d038a4594d5bec5550cf
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-08-08 16:47:46 -0500

    Fix destinations drop menu options

--- src/scripts.js
+++ src/scripts.js
@@ -10,6 +10,7 @@ import './images/turing-logo.png'
 // console.log('This is the JavaScript entry file - your code begins here.');
 // --------------------------------------------------------------->>
 
+///--------------- Import Section -----------------------------///
 import Traveler from './classes/Traveler';
 import Trip from './classes/Trip';
 import Agency from './classes/Agency';
@@ -17,3 +18,27 @@ import Destination from './classes/Destination';
 import fetchCalls from './fetchCalls';
 import domUpdates from './domUpdates';
 import dayjs from 'dayjs';
+
+
+///--------------- Queries Section -----------------------------///
+const logOutBtn = document.getElementById('logOutBtn');
+const submitFormBtn = document.getElementById('submitFormBtn');
+
+
+const travelerGreeting = document.getElementById('travelerGreeting');
+const totalSpentInfo = document.getElementById('totalSpentInfo');
+const planningCost = document.getElementById('planningCost');
+
+const planningDate = document.getElementById('planningDate');
+const planningNoDays = document.getElementById('planningNoDays');
+const planningNoTravelers = document.getElementById('planningNoTravelers');
+const destinationDropdown = document.getElementById('destinationDropdown');
+console.log(destinationDropdown.value);
+
+
+///--------------- Event Listeners -----------------------------///
+
+
+
+
+///--------------- Functions -----------------------------///
