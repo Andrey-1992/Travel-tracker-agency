e5f74d8497cac38437a72af13110cde9224b3262
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-08-08 15:56:06 -0500

    Import classes, fetch calls and dom updates files in scripts.js file

---
+++ src/fetchCalls.js
@@ -0,0 +1,45 @@
+const fetchCalls = {
+
+  getData(endPoint) {
+    return fetch(`http://localhost:3001/api/v1/${endPoint}`)
+      .then(response => response.json())
+      .then(data => data)
+      .catch((error) => this.displayErrorMessage(error));
+  },
+
+  postNewData(endPoint, body) {
+    return fetch(`http://localhost:3001/api/v1/${endPoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) =>
+        this.checkForErrors(res))
+      .catch((error) => this.displayErrorMessage(error, endPoint));
+  },
+
+  checkForErrors(res) {
+    if (!res.ok) {
+      throw new Error("Please make sure all Fields are filled out");
+    }
+    return res.json();
+  },
+
+  displayErrorMessage(error, endPoint) {
+    // const postErrorActivity = document.querySelector(".post-error-activity");
+    // const errorField = document.querySelector('.js-error');
+    //
+    // if (endPoint === 'activity') {
+    //   postErrorActivity.innerText = `${err} -Please check back later.`
+    // }
+    // if (endPoint === 'sleep') {
+    //   errorField.innerHTML = `${err} -Please check back later.`
+    // }
+  },
+
+
+};
+
+export default fetchCalls;
--- src/scripts.js
+++ src/scripts.js
@@ -7,4 +7,13 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// console.log('This is the JavaScript entry file - your code begins here.');
+// --------------------------------------------------------------->>
+
+import Traveler from './classes/Traveler';
+import Trip from './classes/Trip';
+import Agency from './classes/Agency';
+import Destination from './classes/Destination';
+import fetchCalls from './fetchCalls';
+import domUpdates from './domUpdates';
+import dayjs from 'dayjs';
