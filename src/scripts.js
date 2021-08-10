ef450eda3710f6c7437be2af5c52b92bfe59f7da
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-08-10 18:12:26 -0500

    Clean and organize css code

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,3 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-// An example of how you tell webpack to use a CSS (SCSS) file
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// console.log('This is the JavaScript entry file - your code begins here.');
-// --------------------------------------------------------------->>
-
 
 ///--------------- Import Section -----------------------------//
 import './css/base.scss';
@@ -17,8 +10,6 @@ import Destination from './classes/Destination';
 import fetchCalls from './fetchCalls';
 import domUpdates from './domUpdates';
 import dayjs from 'dayjs';
-
-
 
 
 ///--------------- Queries Section -----------------------------///
